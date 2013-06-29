05599e4f7c71f077bfc9a884359974a5abe2d82c
estebanfcv <estebanfcv@localhost.localdomain>
2013-06-29 16:00:56 -0500

    primer commit

---
+++ src/holafedora/HolaFedora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holafedora;
+
+/**
+ *
+ * @author estebanfcv
+ */
+public class HolaFedora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA"+System.getProperty("os.name")+" Version "+System.getProperty("os.version"));
+        // TODO code application logic here
+    }
+}
